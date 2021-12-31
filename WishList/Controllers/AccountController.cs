0809c2b72934a3687f68218ea6342ef06d3dee73
fhalvorsen <fulin.halvorsen@hotmail.com>
2021-12-31 10:40:30 +0100

    added AccountController

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            
+        }
+        
+        // GET
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
